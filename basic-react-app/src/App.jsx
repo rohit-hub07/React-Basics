e0da73474d83887383604cd7c3e3d94737e165f5
rohit singh <rohitsingh986491@gmail.com>
2024-07-27 00:02:43 +0530

    basic react file

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import Title from "./Title.jsx"
+import Description from "./Description.jsx"
+import ProductTab from "./ProductTab.jsx"
+
+function App() {
+  return (
+    <ProductTab/>
+  ); 
+}
+
+export default App
---
+++ basic-react-app/src/Description.jsx
@@ -0,0 +1,5 @@
+function Description() {
+  return <h3>Hi rohit</h3>
+}
+
+export default Description;
